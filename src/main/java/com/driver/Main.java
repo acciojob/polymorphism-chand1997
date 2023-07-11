d7c2177fd3e6ee87ced5c7167767ba81d3bd32e8
chand1997 <chanduchallahalli@gmail.com>
2023-07-11 11:07:20 +0530

    created method product which is taking 3 arguments and called it using object p

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -5,12 +5,16 @@ public class Main {
         public int product(int x,int y){
             return x*y;
         }
+        public int product(int x,int y,int z){
+            return x*y*z;
+        }
 
     }
 
     public static void main(String[] args) {
         Product p=new Product();
         System.out.println(p.product(2,44));
+        System.out.println(p.product(11,33,22));
     }
 
 }
