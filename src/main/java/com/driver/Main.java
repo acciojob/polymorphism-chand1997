bfcce695b815b259b04db8afe03e9e2db1794200
chand1997 <chanduchallahalli@gmail.com>
2023-07-11 11:05:32 +0530

    created method product which is taking 2 arguments and called it using object p

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,11 +2,15 @@ package com.driver;
 
 public class Main {
     public static class Product{
+        public int product(int x,int y){
+            return x*y;
+        }
 
     }
 
     public static void main(String[] args) {
         Product p=new Product();
+        System.out.println(p.product(2,44));
     }
 
 }
